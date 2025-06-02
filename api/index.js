7cee8707e0cc8b5987a533594ce088cc1f05367d
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-06-02 17:17:13 -0500

    se sube back5

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const serverless = require('serverless-http');
 require('dotenv').config();
 
 const authRoutes = require('../controllers/authController');
@@ -10,13 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/register', authRoutes.register);
-app.post('/api/login', authRoutes.login);
-app.post('/api/chat', chatRoutes.sendMessage);
+// Rutas de prueba
 app.get('/', (req, res) => {
   res.send('Funciona backend Serenia ✅');
 });
 
-const serverless = require('serverless-http');
-module.exports.handler = serverless(app);
+// Rutas reales
+app.post('/api/register', authRoutes.register);
+app.post('/api/login', authRoutes.login);
+app.post('/api/chat', chatRoutes.sendMessage);
 
+// Exportar como función serverless
+module.exports = serverless(app);
