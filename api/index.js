d93673c857d4eedc84996c4cc2bf807beef46bb3
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-06-02 17:07:07 -0500

    se sube back2

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const authRoutes = require('../controllers/authController');
+const chatRoutes = require('../controllers/chatController');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/register', authRoutes.register);
+app.post('/api/login', authRoutes.login);
+app.post('/api/chat', chatRoutes.sendMessage);
+
+// Exporta como handler para Vercel
+module.exports = app;
--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendMessage } = require('../controllers/chatController');
-
-router.post('/chat', sendMessage);
-
-module.exports = router;
